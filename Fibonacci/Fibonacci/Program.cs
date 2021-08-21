6d976c875b189be87f56916d7d1555fb536fa3fe
Brittany Pruneau <69804752+BrittanyPruneau@users.noreply.github.com>
2021-08-21 16:01:30 -0600

    Add files via upload

---
+++ Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int previous = 0;
+            int current = 1;
+           
+            int[] fibonacci = new int[25];
+            fibonacci[0] = previous;
+            fibonacci[1] = current;
+            Console.WriteLine(fibonacci[0]);
+            Console.WriteLine(fibonacci[1]);
+             
+
+           for (int i = 2; i<fibonacci.Length; i++)
+            {
+                fibonacci[i]= fibonacci[i-2] + fibonacci[i-1];
+                Console.WriteLine(fibonacci[i]);
+
+
+            }
+        
+        }
+    }
+}
+
